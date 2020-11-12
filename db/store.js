99dcf8b273c4bd9a24a79385e2a6597829c0964c
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-11-11 19:53:35 -0500

    added class store and read / write functions

--- db/store.js
+++ db/store.js
@@ -4,3 +4,17 @@ const fs = require("fs");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
+class Store {
+    constructor() {
+      this.lastId = 0;
+    }
+  
+    read() {
+      return readFileAsync("db/db.json", "utf8");
+    }
+  
+    write(note) {
+      return writeFileAsync("db/db.json", JSON.stringify(note));
+    }
+  
+}
